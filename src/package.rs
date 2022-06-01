c761f61b60f38f0dcab50a4892adf4d0c973f8ce
James MacMahon <james@oxide.computer>
2022-06-01 14:08:43 -0400

    Check destination file if it exists (#13)
    If destination exists, check against size and last modified time. If
    both are the same as what is at the URL, then skip downloading,
    otherwise overwrite.

--- src/package.rs
+++ src/package.rs
@@ -4,12 +4,15 @@
 
 //! Utility for bundling target binaries as tarfiles.
 
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, bail, Result};
+use chrono::{DateTime, FixedOffset, Utc};
+use reqwest::header::{CONTENT_LENGTH, LAST_MODIFIED};
 use serde_derive::Deserialize;
 use std::borrow::Cow;
 use std::convert::TryInto;
 use std::fs::{File, OpenOptions};
 use std::path::{Path, PathBuf};
+use std::str::FromStr;
 use tar::Builder;
 use tokio::io::{AsyncSeekExt, AsyncWriteExt};
 
@@ -18,16 +21,84 @@ const S3_BUCKET: &str = "https://oxide-omicron-build.s3.amazonaws.com";
 // Name for the directory component where downloaded blobs are stored.
 const BLOB: &str = "blob";
 
+#[test]
+fn test_converts() {
+    let content_length = "1966080";
+    let last_modified = "Fri, 30 Apr 2021 22:37:39 GMT";
+
+    let content_length: u64 = u64::from_str(content_length).unwrap();
+    assert_eq!(1966080, content_length);
+
+    let _last_modified: DateTime<FixedOffset> =
+        chrono::DateTime::parse_from_rfc2822(last_modified).unwrap();
+}
+
 // Downloads "source" from S3_BUCKET to "destination".
-async fn download(source: &str, destination: &Path) -> Result<()> {
+pub async fn download(source: &str, destination: &Path) -> Result<()> {
+    let url = format!("{}/{}", S3_BUCKET, source);
+    let client = reqwest::Client::new();
+
+    if destination.exists() {
+        // If destination exists, check against size and last modified time. If
+        // both are the same, then return Ok
+        let head_response = client.head(&url).send().await?;
+        if !head_response.status().is_success() {
+            bail!("head failed! {:?}", head_response);
+        }
+
+        let headers = head_response.headers();
+
+        // From S3, header looks like:
+        //
+        //    "Content-Length: 49283072"
+        let content_length = headers
+            .get(CONTENT_LENGTH)
+            .ok_or_else(|| anyhow!("no content length on {} HEAD response!", url))?;
+        let content_length: u64 = u64::from_str(content_length.to_str()?)?;
+
+        // From S3, header looks like:
+        //
+        //    "Last-Modified: Fri, 27 May 2022 20:50:17 GMT"
+        let last_modified = headers
+            .get(LAST_MODIFIED)
+            .ok_or_else(|| anyhow!("no last modified on {} HEAD response!", url))?;
+        let last_modified: DateTime<FixedOffset> =
+            chrono::DateTime::parse_from_rfc2822(last_modified.to_str()?)?;
+        let metadata = tokio::fs::metadata(&destination).await?;
+        let metadata_modified: DateTime<Utc> = metadata.modified()?.into();
+
+        if metadata.len() == content_length && metadata_modified == last_modified {
+            return Ok(());
+        }
+    }
+
     println!(
         "Downloading {} to {}",
         source,
         destination.to_string_lossy()
     );
-    let response = reqwest::get(format!("{}/{}", S3_BUCKET, source)).await?;
+
+    let response = client.get(url).send().await?;
+
+    // Store modified time from HTTPS response
+    let last_modified = response
+        .headers()
+        .get(LAST_MODIFIED)
+        .ok_or_else(|| anyhow!("no last modified on GET response!"))?;
+    let last_modified: DateTime<FixedOffset> =
+        chrono::DateTime::parse_from_rfc2822(last_modified.to_str()?)?;
+
+    // Write file bytes to destination
     let mut file = tokio::fs::File::create(destination).await?;
     file.write_all(&response.bytes().await?).await?;
+    drop(file);
+
+    // Set destination file's modified time based on HTTPS response
+    filetime::set_file_mtime(
+        destination,
+        filetime::FileTime::from_system_time(last_modified.into()),
+    )?;
+
     Ok(())
 }
 
@@ -264,11 +335,7 @@ impl Package {
             std::fs::create_dir_all(&blobs_path)?;
             for blob in blobs {
                 let blob_path = blobs_path.join(blob);
-                // TODO: Check against hash, download if mismatch (i.e.,
-                // corruption/update).
-                if !blob_path.exists() {
-                    download(&blob.to_string_lossy(), &blob_path).await?;
-                }
+                download(&blob.to_string_lossy(), &blob_path).await?;
                 progress.increment(1);
             }
             archive.append_dir_all(&destination_path, &blobs_path)?;
